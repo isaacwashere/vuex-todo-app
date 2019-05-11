393a167eeb53f2825d7ceb31d41a7cc46ab87d23
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-11 00:25:58 -0700

    added the ability to mark a todo as complete

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,23 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double Click to Mark Complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      <div 
+        @dblclick="onDblClick(todo)" 
+        v-for="todo in allTodos" 
+        :key="todo.id" 
+        class="todo"
+        v-bind:class="{ 'is-complete' : todo.completed }"
+      >
         {{ todo.title }}
         <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
@@ -16,7 +31,16 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos", "deleteTodo"])
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      }
+
+      this.updateTodo(updTodo);
+    }
   },
   computed: mapGetters(['allTodos']),
   created() {
