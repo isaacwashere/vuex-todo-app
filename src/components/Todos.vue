427b3e66ddda58fe2f8437d6e511564ad21dca9f
Isaac Curtiss <ijcurtiss7@gmail.com>
2019-05-10 23:27:08 -0700

    added the ability to delete a todo from backend and in UI

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,7 +2,10 @@
   <div>
     <h3>Todos</h3>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">{{ todo.title }}</div>
+      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+        {{ todo.title }}
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
+      </div>
     </div>
   </div>
 </template>
@@ -13,7 +16,7 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(['fetchTodos'])
+    ...mapActions(["fetchTodos", "deleteTodo"])
   },
   computed: mapGetters(['allTodos']),
   created() {
